3e6ab838473c1e29bc3b6e3ff3c5bceb15ae3f77
Gouda <4hmed.hisham@gmail.com>
2023-02-04 21:21:24 +0200

    Added Timed branch in grey

--- Assets/abilites.cs
+++ Assets/abilites.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class abilites : MonoBehaviour
 {
     public int pressCounter = 0;
+    public float branchTimeout=0;
+    public float branchTimeoutPeriod;
+    private float TimeStart;
+    private bool FirstTime=true;
+    private bool IsPlayerTouchingMe=false;
     public enum Property{
         STONE,
         WEAK_BRANCH,
         HARD_BRANCH,
         THORNS,
-        //TIMED_BRANCH
+        TIMED_BRANCH
     }
     public Property singleProperty=Property.STONE;
     // Start is called before the first frame update
@@ -28,20 +33,47 @@ public class abilites : MonoBehaviour
             case Property.HARD_BRANCH:
             gameObject.GetComponent<SpriteRenderer>().color=new Color(150f/255f,75/255f,0f/255f);
             break;
+            case Property.TIMED_BRANCH:
+            gameObject.GetComponent<SpriteRenderer>().color=Color.gray;
+            break;
         }
-
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        // if(singleProperty==Property.TIMED_BRANCH)
+        // {
+        //     Debug.Log("Time is "+Time.time);
+        // }
+        if(singleProperty==Property.TIMED_BRANCH &&IsPlayerTouchingMe){
+            if(FirstTime){
+            TimeStart=Time.time;
+
+            FirstTime=false;
+            }
+            if (( Time.time > (TimeStart+branchTimeoutPeriod))){
+                        Destroy(gameObject);
+
+                        gameManager.platforms.Remove(gameObject);
+                        
+                    }
+        }
+        
         
     }
     void OnCollisionEnter2D(Collision2D other){
 		//Debug.Log("Player-Platfrom Collision!");
 		if(other.gameObject.tag==KillBehaviour.PLAYER_TAG){
 			Movement.IsGrounded=true;
+            if(singleProperty==Property.TIMED_BRANCH){
+                IsPlayerTouchingMe=true;
+
+            }
+
             //Debug.Log("Grounded set to true");
             switch(singleProperty) 
 
@@ -57,4 +89,8 @@ public class abilites : MonoBehaviour
 
         
 	}
+    void OnTriggerExit2D(Collider2D other){
+        IsPlayerTouchingMe=false;
+        }
+
 }
